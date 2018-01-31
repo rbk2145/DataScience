01066573b1bb4624bf642625c52753137628c73d
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-01-31 07:19:48 +0530

    Create while.py

---
+++ intermediate/4_Loops/while.py
@@ -0,0 +1,29 @@
+####################
+x = 1
+while x < 4 :
+    print(x)
+    x = x + 1
+    
+###############################Basic while loop
+# Initialize offset
+offset = 8
+
+# Code the while loop
+while offset!= 0:
+    print("correcting...")
+    offset = offset-1
+    print(offset)
+
+
+
+############################Add conditionals
+# Initialize offset
+offset = -6
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    if offset>0:
+        offset=offset+1
+    
+    print(offset)
