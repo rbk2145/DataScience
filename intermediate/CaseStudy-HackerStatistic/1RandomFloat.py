a07cab24c76e608c5eb7c246dc4919b2a585a375
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-02-01 23:28:44 +0530

    Update 1RandomFloat.py

--- intermediate/CaseStudy-HackerStatistic/1RandomFloat.py
+++ intermediate/CaseStudy-HackerStatistic/1RandomFloat.py
@@ -19,4 +19,23 @@ print (np.random.randint(1,7))
 # Use randint() again
 print(np.random.randint(1,7))
 
+####
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <= 5 :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
 
