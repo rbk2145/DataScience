d9f22539f23697c19b0e3ca1a9f0d1dc04932e92
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-02-04 23:59:38 +0530

    Create 1_Iterating over iterables .py

---
+++ 4ToolBox_2/Iterators/1_Iterating over iterables .py
@@ -0,0 +1,62 @@
+###Iterating over iterables (1)
+# Create a list of strings: flash
+flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
+
+# Print each list item in flash using a for loop
+for person in flash:
+    print(person)
+# Create an iterator for flash: superspeed
+superspeed = iter(person)
+# Print each item from the iterator
+print(next(superspeed))
+print(next(superspeed))
+print(next(superspeed))
+print(next(superspeed))
+
+
+###2
+# Create an iterator for range(3): small_value
+small_value = iter(range(3))
+
+# Print the values in small_value
+print(next(small_value))
+print(next(small_value))
+print(next(small_value))
+
+# Loop over range(3) and print the values
+for num in range(3):
+    print(num)
+
+
+# Create an iterator for range(10 ** 100): googol
+googol = iter(range(10**100))
+
+# Print the first 5 values from googol
+print(next(googol))
+print(next(googol))
+print(next(googol))
+print(next(googol))
+print(next(googol))
+
+
+##############3Iterators as function arguments
+# Create a range object: values
+values = range(10,21)
+
+# Print the range object
+print(values)
+
+# Create a list of integers: values_list
+values_list = list(values)
+
+# Print values_list
+print(values_list)
+
+# Get the sum of values: values_sum
+values_sum = sum(values)
+
+# Print values_sum
+print(values_sum)
+
+
+###############
