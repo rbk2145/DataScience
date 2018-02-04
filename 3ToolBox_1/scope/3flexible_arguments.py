0eb45c35352da27ab0e0c8f16e986d4f888bf0ed
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-02-04 14:36:16 +0530

    Create 3flexible_arguments.py

---
+++ 3ToolBox_1/scope/3flexible_arguments.py
@@ -0,0 +1,107 @@
+##Functions with one default argument
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+     exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = echo *word1
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+# Call shout_echo() with "Hey": no_echo
+no_echo = shout_echo("Hey")
+
+# Call shout_echo() with "Hey" and echo=5: with_echo
+with_echo = shout_echo("Hey", echo=5)
+
+# Print no_echo and with_echo
+print(no_echo)
+print(with_echo)
+
+
+###Functions with multiple default arguments
+# Define shout_echo
+def shout_echo(word1,echo=1, intense = False):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Capitalize echo_word if intense is True
+    if intense is True:
+        # Capitalize and concatenate '!!!': echo_word_new
+        echo_word_new = echo_word.upper() + '!!!'
+    else:
+        # Concatenate '!!!' to echo_word: echo_word_new
+        echo_word_new = echo_word + '!!!'
+
+    # Return echo_word_new
+    return echo_word_new
+
+# Call shout_echo() with "Hey", echo=5 and intense=True: with_big_echo
+with_big_echo = shout_echo("Hey",echo=5,intense=True)
+
+# Call shout_echo() with "Hey" and intense=True: big_no_echo
+big_no_echo = shout_echo("Hey",intense=True)
+
+# Print values
+print(with_big_echo)
+print(big_no_echo)
+
+
+###Function with variable-length arguments (*args)
+# Define gibberish
+def gibberish(*args):
+    """Concatenate strings in *args together."""
+
+    # Initialize an empty string: hodgepodge
+    hodgepodge = ''
+
+    # Concatenate the strings in args
+    for word in args:
+        hodgepodge += word
+
+    # Return hodgepodge
+    return hodgepodge
+
+# Call gibberish() with one string: one_word
+one_word = gibberish("luke")
+
+
+# Call gibberish() with five strings: many_words
+many_words = gibberish("luke", "leia", "han", "obi", "darth")
+
+# Print one_word and many_words
+print(one_word)
+print(many_words)
+
+###Function with variable-length keyword arguments (**kwargs)
+
+# Define report_status
+def report_status(**kwargs):
+    """Print out the status of a movie character."""
+
+    print("\nBEGIN: REPORT\n")
+
+    # Iterate over the key-value pairs of kwargs
+    for i, j in kwargs.items():
+        # Print out the keys and values, separated by a colon ':'
+        print(i + ": " + j)
+
+    print("\nEND REPORT")
+
+# First call to report_status()
+report_status(name="luke",affiliation="jedi",status="missing")
+
+
+# Second call to report_status()
+report_status(name="anakin", affiliation="sith lord", status="deceased")
+
+
+
