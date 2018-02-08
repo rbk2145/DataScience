f78f0db6c9ababea05ff84da81db347161ee0430
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-02-08 07:29:19 +0530

    Create 2_flatfiles_in_ds.py

---
+++ 5_Importing Data in Python1/1_Introduction and flat files/2_flatfiles_in_ds.py
@@ -0,0 +1,10 @@
+A relational database (e.g. PostgreSQL) is not a flat file
+
+Flat files consist of multiple tables with structured relationships between the tables. Is FALSE
+Flat files consist of rows and each row is called a record. IS TRUE
+A record in a flat file is composed of fields or attributes, each of which contains at most one item of information. IS TRUE
+Flat files are pervasive in data science. IS TRUE
+
+
+
+
