e0981174879c568121fb215b9b17cfec53619896
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-02-13 22:27:43 +0530

    Create 2_drop.py

---
+++ 7_Cleaning Data in Python/1_Exploring your data/2_drop.py
@@ -0,0 +1,13 @@
+###Frequency counts for categorical data
+# Print the value counts for 'Borough'
+print(df['Borough'].value_counts(dropna=False))
+
+# Print the value_counts for 'State'
+print(df["State"].value_counts(dropna=False))
+
+# Print the value counts for 'Site Fill'
+print(df["Site Fill"].value_counts(dropna=False))
+
+
+
+###
