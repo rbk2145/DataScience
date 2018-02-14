f240a4dff564df43680483563b01b9dc6c417a7c
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-02-15 02:22:49 +0530

    Create 3_Beyond melt and pivot.py

---
+++ 7_Cleaning Data in Python/2_Tidying data for analysis/3_Beyond melt and pivot.py
@@ -0,0 +1,31 @@
+###1
+###Splitting a column with .str
+# Melt tb: tb_melt
+tb_melt = pd.melt(tb, id_vars=['country', 'year'])
+
+# Create the 'gender' column
+tb_melt['gender'] = tb_melt.variable.str[0]
+
+# Create the 'age_group' column
+tb_melt['age_group'] = tb_melt.variable.str[1:]
+
+# Print the head of tb_melt
+print(tb_melt.head())
+
+
+###2
+###Splitting a column with .split() and .get()
+# Melt ebola: ebola_melt
+ebola_melt = pd.melt(ebola, id_vars=['Date', 'Day'], var_name='type_country', value_name='counts')
+
+# Create the 'str_split' column
+ebola_melt['str_split'] = ebola_melt.type_country.str.split('_')
+
+# Create the 'type' column
+ebola_melt['type'] = ebola_melt.str_split.str.get(0)
+
+# Create the 'country' column
+ebola_melt['country'] = ebola_melt.str_split.str.get(1)
+
+# Print the head of ebola_melt
+print(ebola_melt.head())
