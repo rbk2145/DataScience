d910620f9ae66bf51b1308c3c11e37cac4269cc6
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-02-21 09:32:02 +0530

    Create 2_Building DataFrames from scratch.py

---
+++ 8_pandas Foundations/1_Data ingestion - inspection/2_Building DataFrames from scratch.py
@@ -0,0 +1,40 @@
+##Zip lists to build a DataFrame
+# Zip the 2 lists together into one list of (key,value) tuples: zipped
+zipped = list(zip(list_keys,list_values))
+
+# Inspect the list using print()
+print(zipped)
+
+# Build a dictionary with the zipped list: data
+data = dict(zipped)
+
+# Build and inspect a DataFrame from the dictionary: df
+df = pd.DataFrame(data)
+print(df)
+
+
+###Labeling your data
+# Build a list of labels: list_labels
+list_labels = ["year","artist","song","chart weeks"]
+
+# Assign the list of labels to the columns attribute: df.columns
+df.columns = list_labels
+
+
+
+###Building DataFrames with broadcasting
+# Make a string with the value 'PA': state
+state = "PA"
+
+# Construct a dictionary: data
+data = {'state':state, 'city':cities}
+
+# Construct a DataFrame from dictionary data: df
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
+
+
+
+###
