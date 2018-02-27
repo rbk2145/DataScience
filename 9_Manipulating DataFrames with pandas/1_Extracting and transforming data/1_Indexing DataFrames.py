ed8a83d5b053cc5435b48ae883dc6b07eb8c9eff
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-02-27 23:35:40 +0530

    Create 1_Indexing DataFrames.py

---
+++ 9_Manipulating DataFrames with pandas/1_Extracting and transforming data/1_Indexing DataFrames.py
@@ -0,0 +1,26 @@
+###Positional and labeled indexing
+# Assign the row position of election.loc['Bedford']: x
+x = 4
+
+# Assign the column position of election['winner']: y
+y = 4
+
+# Print the boolean equivalence
+print(election.iloc[x, y] == election.loc['Bedford', 'winner'])
+
+
+###Indexing and column rearrangement
+# Import pandas
+import pandas as pd
+
+# Read in filename and set the index: election
+election = pd.read_csv(filename, index_col='county')
+
+# Create a separate dataframe with the columns ['winner', 'total', 'voters']: results
+results = election[['winner', 'total', 'voters']]
+
+# Print the output of results.head()
+print(results.head())
+
+
+###
