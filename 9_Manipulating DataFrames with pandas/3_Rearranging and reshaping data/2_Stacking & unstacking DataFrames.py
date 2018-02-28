c6c7510b213d6b66f8ca78968200d2d59ecc14a6
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-02-28 22:44:37 +0530

    Create 2_Stacking & unstacking DataFrames.py

---
+++ 9_Manipulating DataFrames with pandas/3_Rearranging and reshaping data/2_Stacking & unstacking DataFrames.py
@@ -0,0 +1,43 @@
+###Stacking & unstacking I
+# Unstack users by 'weekday': byweekday
+byweekday = users.unstack(level='weekday')
+
+# Print the byweekday DataFrame
+print(byweekday)
+
+# Stack byweekday by 'weekday' and print it
+print(byweekday.stack(level='weekday'))
+
+
+###Stacking & unstacking II
+# Unstack users by 'city': bycity
+bycity = users.unstack(level='city')
+
+# Print the bycity DataFrame
+print(bycity)
+
+# Stack bycity by 'city' and print it
+print(bycity.stack(level='city'))
+
+
+###Restoring the index order
+# Stack 'city' back into the index of bycity: newusers
+newusers = bycity.stack(level='city')
+
+# Swap the levels of the index of newusers: newusers
+newusers = newusers.swaplevel(0, 1)
+
+# Print newusers and verify that the index is not sorted
+print(newusers)
+
+# Sort the index of newusers: newusers
+newusers = newusers.sort_index()
+
+# Print newusers and verify that the index is now sorted
+print(newusers)
+
+# Verify that the new DataFrame is equal to the original
+print(newusers.equals(users))
+
+
+###
