917af5b7b96a39d1a8e2c3d21d161a0c68ae59d0
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-03-02 23:26:02 +0530

    Create 1_Categoricals and groupby.py

---
+++ 9_Manipulating DataFrames with pandas/4_Grouping data/1_Categoricals and groupby.py
@@ -0,0 +1,35 @@
+###Grouping by multiple columns
+# Group titanic by 'pclass'
+by_class = titanic.groupby('pclass')
+
+# Aggregate 'survived' column of by_class by count
+count_by_class = by_class['survived'].count()
+
+# Print count_by_class
+print(count_by_class)
+
+# Group titanic by 'embarked' and 'pclass'
+by_mult = titanic.groupby(['embarked','pclass'])
+
+# Aggregate 'survived' column of by_mult by count
+count_mult = by_mult['survived'].count()
+
+# Print count_mult
+print(count_mult)
+
+
+###Grouping by another series
+# Read life_fname into a DataFrame: life
+life = pd.read_csv(life_fname, index_col='Country')
+
+# Read regions_fname into a DataFrame: regions
+regions = pd.read_csv(regions_fname, index_col='Country')
+
+# Group life by regions['region']: life_by_region
+life_by_region = life.groupby(regions['region'])
+
+# Print the mean over the '2010' column of life_by_region
+print(life_by_region['2010'].mean())
+
+
+###
