b50f5da836a0523f5ff2f49d27cfcf993e51673f
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-03-02 23:45:28 +0530

    Create 2_Groupby and aggregation.py

---
+++ 9_Manipulating DataFrames with pandas/4_Grouping data/2_Groupby and aggregation.py
@@ -0,0 +1,50 @@
+###Computing multiple aggregates of multiple columns
+# Group titanic by 'pclass': by_class
+by_class = titanic.groupby('pclass')
+
+# Select 'age' and 'fare'
+by_class_sub = by_class[['age','fare']]
+
+# Aggregate by_class_sub by 'max' and 'median': aggregated
+aggregated = by_class_sub.agg(['max','median'])
+
+# Print the maximum age in each class
+print(aggregated.loc[:, ('age','max')])
+
+# Print the median fare in each class
+print(aggregated.loc[:, ('fare','median')])
+
+
+####Aggregating on index levels/fields
+a# Read the CSV file into a DataFrame and sort the index: gapminder
+gapminder = pd.read_csv('gapminder.csv', index_col=['Year','region','Country']).sort_index()
+
+# Group gapminder by 'Year' and 'region': by_year_region
+by_year_region = gapminder.groupby(level=['Year','region'])
+
+# Define the function to compute spread: spread
+def spread(series):
+    return series.max() - series.min()
+
+# Create the dictionary: aggregator
+aggregator = {'population':'sum', 'child_mortality':'mean', 'gdp':spread}
+
+# Aggregate by_year_region using the dictionary: aggregated
+aggregated = by_year_region.agg(aggregator)
+
+# Print the last 6 entries of aggregated 
+print(aggregated.tail(6))
+
+
+###Grouping on a function of the index
+# Read file: sales
+sales = pd.read_csv('sales.csv', index_col='Date', parse_dates=True)
+
+# Create a groupby object: by_day
+by_day = sales.groupby(sales.index.strftime('%a'))
+
+# Create sum: units_sum
+units_sum = by_day['Units'].sum()
+
+# Print units_sum
+print(units_sum)
