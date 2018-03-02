cd57a0c985451ae24717a1053ed49e8f9fd071f3
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-03-02 23:51:03 +0530

    Create 4_Groupby and filtering.py

---
+++ 9_Manipulating DataFrames with pandas/4_Grouping data/4_Groupby and filtering.py
@@ -0,0 +1,40 @@
+###Grouping and filtering with .apply()
+# Create a groupby object using titanic over the 'sex' column: by_sex
+by_sex = titanic.groupby('sex')
+
+# Call by_sex.apply with the function c_deck_survival and print the result
+c_surv_by_sex = by_sex.apply(c_deck_survival)
+
+# Print the survival rates
+print(c_surv_by_sex)
+
+
+
+###Grouping and filtering with .filter()
+# Read the CSV file into a DataFrame: sales
+sales = pd.read_csv('sales.csv', index_col='Date', parse_dates=True)
+
+# Group sales by 'Company': by_company
+by_company = sales.groupby('Company')
+
+# Compute the sum of the 'Units' of by_company: by_com_sum
+by_com_sum = by_company['Units'].sum()
+print(by_com_sum)
+
+# Filter 'Units' where the sum is > 35: by_com_filt
+by_com_filt = by_company.filter(lambda g:g['Units'].sum() > 35)
+print(by_com_filt)
+
+
+###Filtering and grouping with .map()
+# Create the Boolean Series: under10
+under10 = (titanic['age'] < 10).map({True:'under 10', False:'over 10'})
+
+# Group by under10 and compute the survival rate
+survived_mean_1 = titanic.groupby(under10)['survived'].mean()
+print(survived_mean_1)
+
+# Group by under10 and pclass and compute the survival rate
+survived_mean_2 = titanic.groupby([under10, 'pclass'])['survived'].mean()
+print(survived_mean_2)
+
