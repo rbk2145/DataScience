939fc2a0fd295f759c7985a8412881ddee71537e
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-03-03 15:29:31 +0530

    Create 2_Reindexing DataFrames.py

---
+++ 010_Merging DataFrames with pandas/1_Preparing data/2_Reindexing DataFrames.py
@@ -0,0 +1,63 @@
+####Sorting DataFrame with the Index & columns
+# Import pandas
+import pandas as pd
+
+# Read 'monthly_max_temp.csv' into a DataFrame: weather1
+weather1 = pd.read_csv('monthly_max_temp.csv', index_col='Month')
+
+# Print the head of weather1
+print(weather1.head())
+
+# Sort the index of weather1 in alphabetical order: weather2
+weather2 = weather1.sort_index()
+
+# Print the head of weather2
+print(weather2.head())
+
+# Sort the index of weather1 in reverse alphabetical order: weather3
+weather3 = weather1.sort_index(ascending=False)
+
+# Print the head of weather3
+print(weather3.head())
+
+# Sort weather1 numerically using the values of 'Max TemperatureF': weather4
+weather4 = weather1.sort_values('Max TemperatureF')
+
+# Print the head of weather4
+print(weather4.head())
+
+
+
+####Reindexing DataFrame from a list
+# Import pandas
+import pandas as pd
+
+# Reindex weather1 using the list year: weather2
+weather2 = weather1.reindex(year)
+
+# Print weather2
+print(weather2)
+
+# Reindex weather1 using the list year with forward-fill: weather3
+weather3 = weather1.reindex(year).ffill()
+
+# Print weather3
+print(weather3)
+
+
+
+####Reindexing using another DataFrame Index
+# Import pandas
+import pandas as pd
+
+# Reindex names_1981 with index of names_1881: common_names
+common_names = names_1981.reindex(names_1881.index)
+
+# Print shape of common_names
+print(common_names.shape)
+
+# Drop rows with null counts: common_names
+common_names = common_names.dropna()
+
+# Print shape of new common_names
+print(common_names.shape)
