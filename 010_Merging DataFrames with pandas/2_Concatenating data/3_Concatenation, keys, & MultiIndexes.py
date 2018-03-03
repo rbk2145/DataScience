f91fc2e508c3a12c8e05daa639f4e0ab8a3c28c3
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2018-03-04 00:06:23 +0530

    Create 3_Concatenation, keys, & MultiIndexes.py

---
+++ 010_Merging DataFrames with pandas/2_Concatenating data/3_Concatenation, keys, & MultiIndexes.py
@@ -0,0 +1,76 @@
+####Concatenating vertically to get MultiIndexed rows
+for medal in medal_types:
+
+    file_name = "%s_top5.csv" % medal
+
+    # Read file_name into a DataFrame: medal_df
+    medal_df = pd.read_csv(file_name, index_col='Country')
+    
+    # Append medal_df to medals
+    medals.append(medal_df)
+
+# Concatenate medals: medals
+medals = pd.concat(medals, keys=['bronze', 'silver', 'gold'])
+
+# Print medals
+print(medals)
+
+
+
+####Slicing MultiIndexed DataFrames
+# Sort the entries of medals
+medals_sorted = medals.sort_index(level=0)
+
+# Print the number of Bronze medals won by Germany
+print(medals_sorted.loc[('bronze','Germany')])
+
+# Print data about silver medals
+print(medals_sorted.loc['silver'])
+
+# Create alias for pd.IndexSlice: idx
+idx = pd.IndexSlice
+
+# Print all the data on medals won by the United Kingdom
+print(medals_sorted.loc[idx[:,'United Kingdom'], :])
+
+
+
+####Concatenating horizontally to get MultiIndexed columns
+# Concatenate dataframes: february
+february = pd.concat(dataframes, keys=['Hardware','Software','Service'], axis=1)
+
+# Print february.info()
+print(february.info())
+
+# Assign pd.IndexSlice: idx
+idx = pd.IndexSlice
+
+# Create the slice: slice_2_8
+slice_2_8 = february.loc['2015-2-2':'2015-2-8', idx[:,'Company']]
+
+# Print slice_2_8
+print(slice_2_8)
+
+
+
+####Concatenating DataFrames from a dict
+# Make the list of tuples: month_list
+month_list = [('january', jan), ('february', feb), ('march', mar)]
+
+# Create an empty dictionary: month_dict
+month_dict = {}
+
+for month_name, month_data in month_list:
+
+    # Group month_data: month_dict[month_name]
+    month_dict[month_name] = month_data.groupby('Company').sum()
+
+# Concatenate data in month_dict: sales
+sales = pd.concat(month_dict)
+
+# Print sales
+print(sales)
+
+# Print all sales by Mediacore
+idx = pd.IndexSlice
+print(sales.loc[idx[:, 'Mediacore'], :])
